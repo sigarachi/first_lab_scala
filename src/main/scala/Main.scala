d0c5431f6c5d15f659713298d7cfc153fbbf4525
sigarachi <lalalabtv@gmail.com~>
2023-12-04 21:02:01 +0300

    Генерация чисел

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,17 @@
+import scala.util.Random
+
 object Main {
   private def scalarProduct(xs: List[Double], ys: List[Double]): Double = {
     (xs zip ys).map { case (x, y) => x * y }.sum
   }
 
+  private def generateRandomList(length: Int): List[Double] = {
+    List.fill(length)(Random.nextDouble())
+  }
+
   def main(args: Array[String]): Unit = {
-    val xs = List(1.0, 2.0, 10.0)
-    val ys = List(4.0, 5.0, 6.0)
+    val xs = this.generateRandomList(3)
+    val ys = this.generateRandomList(3)
 
     val result = this.scalarProduct(xs, ys)
     println(result)
